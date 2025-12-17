3a5bcd024345a9ce0850e6fb18b3baa7d40af45e
majid-tech <majidmohammed044@gmail.com>
2025-12-17 00:03:59 +0000

    referenced all tablez

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -30,7 +30,7 @@ CREATE TABLE Orders (
 
 CREATE TABLE Order_Details (
     orderdetailid INT PRIMARY KEY,
-    order_id FOREIGN KEY ,
-    book_id FOREIGN KEY,
+    order_id FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    book_id FOREIGN KEY (book_id) REFERENCES Books(book_id),
     quantity DOUBLE
 );
